90824abcc9e44ad7ddf4529955bc6fa1f1a44edc
ajcumine <ajcumine@gmail.com>
2019-04-30 11:49:58 +0100

    single level evolution chain

--- src/Page/Pokemon.elm
+++ src/Page/Pokemon.elm
@@ -43,12 +43,20 @@ type alias Species =
     }
 
 
+type alias Evolution =
+    { name : String }
+
+
+type alias Evolutions =
+    List Evolution
+
+
 type alias Pokemon =
     { name : String
     , order : Int
     , types : List PokemonType
     , sprites : Sprites
-    , evolutionChainUrl : String
+    , evolutionChain : Evolutions
     }
 
 
@@ -61,9 +69,23 @@ init =
 --VIEW
 
 
-viewPokemonType : PokemonType -> Styled.Html msg
-viewPokemonType pokemonType =
-    Styled.div []
+viewEvolution : Evolution -> Styled.Html msg
+viewEvolution evolution =
+    Styled.div
+        [ css
+            [ textTransform capitalize
+            ]
+        ]
+        [ Styled.text evolution.name ]
+
+
+viewType : PokemonType -> Styled.Html msg
+viewType pokemonType =
+    Styled.div
+        [ css
+            [ textTransform capitalize
+            ]
+        ]
         [ Styled.text pokemonType.name ]
 
 
@@ -71,14 +93,20 @@ viewPokemonDetails : Pokemon -> Styled.Html msg
 viewPokemonDetails pokemon =
     Styled.div []
         [ Styled.h2
-            [ css [ margin3 (px 0) auto (px 20) ]
+            [ css
+                [ margin3 (px 0) auto (px 20)
+                , textTransform capitalize
+                ]
             ]
             [ Styled.text (String.fromInt pokemon.order ++ ": " ++ pokemon.name) ]
         , Styled.img [ src pokemon.sprites.default ] []
         , Styled.img [ src pokemon.sprites.shiny ] []
+        , Styled.h3 [] [ Styled.text "Types" ]
         , Styled.div []
-            (List.map viewPokemonType pokemon.types)
-        , Styled.text pokemon.evolutionChainUrl
+            (List.map viewType pokemon.types)
+        , Styled.h3 [] [ Styled.text "Evolutions" ]
+        , Styled.div []
+            (List.map viewEvolution pokemon.evolutionChain)
         ]
 
 
@@ -92,10 +120,6 @@ viewPokemon model =
             Styled.text "Loading Pokemon..."
 
         RemoteData.Failure error ->
-            let
-                errorLog =
-                    Debug.log "Error" error
-            in
             Styled.text "There was an error fetching your Pokemon"
 
         RemoteData.Success pokemon ->
@@ -153,28 +177,56 @@ pokemonDecoder =
 -- TASK BASED DATA FETCH
 
 
+evolutionDecoder : Decoder Evolution
+evolutionDecoder =
+    Decode.succeed Evolution
+        |> Pipeline.optionalAt [ "species", "name" ] Decode.string "NONE"
+
+
+evolutionsDecoder : Decoder Evolutions
+evolutionsDecoder =
+    Decode.at [ "chain", "evolves_to" ] (Decode.list evolutionDecoder)
+
+
 speciesDecoder : Decoder Species
 speciesDecoder =
     Decode.succeed Species
         |> Pipeline.requiredAt [ "evolution_chain", "url" ] Decode.string
 
 
-buildPokemon pokemon species =
+buildPokemon pokemon evolutionChain =
     { name = pokemon.name
     , order = pokemon.order
     , types = pokemon.types
     , sprites = pokemon.sprites
-    , evolutionChainUrl = species.evolutionChainUrl
+    , evolutionChain = evolutionChain
     }
 
 
-buildPokemonResponse : WebData BasePokemon -> WebData Species -> WebData Pokemon
-buildPokemonResponse pokemonResponse speciesResponse =
-    RemoteData.map2 buildPokemon pokemonResponse speciesResponse
+buildPokemonResponse : WebData BasePokemon -> WebData Evolutions -> WebData Pokemon
+buildPokemonResponse pokemonResponse evolutionsResponse =
+    RemoteData.map2 buildPokemon pokemonResponse evolutionsResponse
 
 
 
 -- HTTP
+
+
+getEvolutions : String -> Task () (WebData Evolutions)
+getEvolutions order =
+    getSpecies order
+        |> Task.andThen
+            (\speciesData ->
+                case speciesData of
+                    RemoteData.Success result ->
+                        RemoteData.Http.getTask result.evolutionChainUrl evolutionsDecoder
+
+                    RemoteData.Failure error ->
+                        Task.fail error |> RemoteData.fromTask
+
+                    _ ->
+                        Task.succeed [] |> RemoteData.fromTask
+            )
 
 
 getSpecies : String -> Task () (WebData Species)
@@ -189,7 +241,7 @@ getPokemon order =
 
 fetch : String -> Cmd Model
 fetch order =
-    Task.map2 buildPokemonResponse (getPokemon order) (getSpecies order)
+    Task.map2 buildPokemonResponse (getPokemon order) (getEvolutions order)
         |> Task.attempt
             (\result ->
                 case result of
